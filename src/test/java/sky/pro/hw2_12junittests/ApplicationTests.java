e4a46ed80f38c76234a9cefac56c565641b21ebe
Vasily Demin <vasily.demin@gmail.com>
2022-07-04 21:42:19 +0300

    Initial commit

---
+++ src/test/java/sky/pro/hw2_12junittests/ApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.hw2_12junittests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
